936510add2c4d331838c4157bf58a06dc49cbb56
karl <none>
2022-06-02 16:05:43 +0100

    User friendly cnx settings / About file This is to be ready for github push, to make installation easier

--- php/cnx.php
+++ php/cnx.php
@@ -2,14 +2,34 @@
 
 
 
+### What's the database name?
+$Base_DName = "budget_app";
 
-$conn = NEW mysqli("localhost", "karl", "password1", "budget_app");
+### What's the username for the database?
+$Base_UName = "karl";
+
+### And the password?
+$Base_PWord = "password1";
+
+### What about the location of your database?
+### If you are hosting the database on the same computer/server.,
+### it is likely you do not need to change
+$Base_Host = "localhost";
+
+
+
+
+## Do no edit below this line ##
+
+
+$conn = NEW mysqli($Base_Host, $Base_UName, $Base_PWord, $Base_DName);
+
+
 
 // Check connection
 if ($conn->connect_error) { // if there is a connection error
     die("Oops ... Connection failed: " . $conn->connect_error);
 }
-//echo "<!--Connected successfully -->"; // let's me know whether this was successful in comments
 
 
 

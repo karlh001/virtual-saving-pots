a6119fa0c21cd71931fa36e4c66ca812b2239e18
karl <none>
2022-06-02 16:37:06 +0100

    Copyright change

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
          style="background-color: rgba(0, 0, 0, 0.2)"
          >
       &copy; 2022
-      <a class="text-dark" href="#">Karl Hunter</a>
+      <a class="text-dark" href="https://github.com/karlh001/virtual-saving-pots">karlh001</a>
     </div>
     <!-- Copyright -->
   </footer>
